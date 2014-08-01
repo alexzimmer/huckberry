a188e431c6c92b004b273f094eda25b641ed4119
Alex <azimmer@gmail.com>
2014-08-01 09:02:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huckberry::Application.config.secret_key_base = '08731248c53279419d0ab2343dbadcc7bb2f52c0ce3a708c2d13a6f30afea25c4a4d6820d5a699213f05791a153394c80cd54a78c5f39c4b00e0c4eed6aa4d85'
